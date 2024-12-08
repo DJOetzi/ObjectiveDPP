88442963ada6a87d00a58588324a66b023a0e470
DJ::Ötzi <maxgabri@icloud.com>
2024-12-09 00:27:38 +0100

    Update DPPCommand.h
    change protocol to include more functions

--- src/commands/include/Protocols/DPPCommand.h
+++ src/commands/include/Protocols/DPPCommand.h
@@ -4,5 +4,12 @@
 #include <dpp/dpp.h>
 
 @protocol DPPCommand
+    - (id) init     : (const std::string&) desc andParameters : (const std::vector<dpp::command_option>) params;
     - (void) exec : (dpp::cluster&) ctx andSlashCommand : (dpp::slashcommand_t&) cmd;
+
+    - (const std::string&) getDescription;
+    - (void) setDescription : (const std::string&)desc;
+
+    - (const std::vector<dpp::command_option>&) getParameters;
+    - (void) setParameters : (const std::vector<dpp::command_option>)params;
 @end
