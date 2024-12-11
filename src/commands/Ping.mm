b0cb5e74aac92a9cdf63d721befc3f531576f9a8
DJ::Ötzi <maxgabri@icloud.com>
2024-12-11 18:38:19 +0100

    Update Ping.mm
    add ping-logging

--- src/commands/Ping.mm
+++ src/commands/Ping.mm
@@ -3,6 +3,7 @@
 @implementation Ping : BaseCommand
     - (void) exec : (dpp::cluster&) ctx andSlashCommand : (const dpp::slashcommand_t&) cmd {
         float ws_ping = ctx.get_shard(0)->websocket_ping;
+        float total_ping = (ctx.rest_ping + ws_ping) * 1000;
 
         /* Ping reply */
         cmd.reply(
@@ -11,9 +12,13 @@
                                 set_color(dpp::colors::yellow).
                                 set_title("PONG!").
                                 set_description(
-                                std::format("Total Ping is: {0:.02f} ms", (ctx.rest_ping + ws_ping) * 1000)
+                                std::format("Total Ping is: {0:.02f} ms", total_ping)
                         )
                 )
         );
+
+        @autoreleasepool {
+            NSLog(@"Total Ping is %s ms", std::to_string(total_ping).c_str());
+        }
     }
 @end
