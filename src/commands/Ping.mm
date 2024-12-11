175345682072d0b15224c98ae7a03f0c95319978
DJ::Ötzi <maxgabri@icloud.com>
2024-12-11 18:35:43 +0100

    Update Ping.mm

--- src/commands/Ping.mm
+++ src/commands/Ping.mm
@@ -2,6 +2,18 @@
 
 @implementation Ping : BaseCommand
     - (void) exec : (dpp::cluster&) ctx andSlashCommand : (const dpp::slashcommand_t&) cmd {
-        cmd.reply("Objective Pong!");
+        float ws_ping = ctx.get_shard(0)->websocket_ping;
+
+        /* Ping reply */
+        cmd.reply(
+                dpp::message().add_embed(
+                        dpp::embed().
+                                set_color(dpp::colors::yellow).
+                                set_title("PONG!").
+                                set_description(
+                                std::format("Total Ping is: {0:.02f} ms", (ctx.rest_ping + ws_ping) * 1000)
+                        )
+                )
+        );
     }
 @end
